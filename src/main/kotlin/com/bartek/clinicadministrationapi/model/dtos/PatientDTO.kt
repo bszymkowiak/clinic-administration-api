deb143a9955eaaf10a92c88e80863a4467292b74
Szymek <szymkowi.bartek@gmail.com>
2021-03-15 13:53:38 +0100

    [DEV-001] Add hibernate configuration

--- src/main/kotlin/com/bartek/clinicadministrationapi/model/dtos/PatientDTO.kt
+++ src/main/kotlin/com/bartek/clinicadministrationapi/model/dtos/PatientDTO.kt
@@ -1,10 +1,15 @@
 package com.bartek.clinicadministrationapi.model.dtos
 
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
 @Entity
 data class PatientDTO (
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int,
     val firstName: String,
     val lastName: String,
