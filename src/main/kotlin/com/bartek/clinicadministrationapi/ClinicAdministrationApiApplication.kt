3964d9147dcb83fab5e23afa6b30ebae66b0f274
Szymek <szymkowi.bartek@gmail.com>
2021-03-15 11:31:26 +0100

    Adding project

---
+++ src/main/kotlin/com/bartek/clinicadministrationapi/ClinicAdministrationApiApplication.kt
@@ -0,0 +1,11 @@
+package com.bartek.clinicadministrationapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClinicAdministrationApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClinicAdministrationApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/bartek/clinicadministrationapi/ClinicAdministrationApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bartek.clinicadministrationapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClinicAdministrationApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
