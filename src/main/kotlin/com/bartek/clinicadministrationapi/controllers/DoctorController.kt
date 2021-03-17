630cc186fdc7bfb86009bd9cd0de1ec1157d5a5a
Szymek <szymkowi.bartek@gmail.com>
2021-03-17 13:07:32 +0100

    [DEV-002] Add doctor methods

--- src/main/kotlin/com/bartek/clinicadministrationapi/controllers/DoctorController.kt
+++ src/main/kotlin/com/bartek/clinicadministrationapi/controllers/DoctorController.kt
@@ -1,12 +1,35 @@
 package com.bartek.clinicadministrationapi.controllers
 
-import com.bartek.clinicadministrationapi.mappers.DoctorMapper
+import com.bartek.clinicadministrationapi.domain.dtos.DoctorDTO
 import com.bartek.clinicadministrationapi.services.DoctorService
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
-class DoctorController (
-    val service: DoctorService,
-    val doctorMapper: DoctorMapper) {
+class DoctorController(
+    val service: DoctorService
+) {
+
+    @GetMapping("/doctors/{id}")
+    fun getDoctorById(@PathVariable id: Long): ResponseEntity<DoctorDTO> {
+        return service.findDoctorById(id)
+    }
+
+    @PostMapping("/doctors")
+    fun addDoctor(@RequestBody doctorDTO: DoctorDTO): ResponseEntity<DoctorDTO> {
+        return service.addDoctorToDb(doctorDTO)
+    }
+
+    @DeleteMapping("/doctors/{id}")
+    fun deleteDoctor(@PathVariable id: Long): ResponseEntity<DoctorDTO> {
+        return service.deleteDoctorById(id)
+    }
+
+    @PutMapping("/doctors")
+    fun updateDoctor(@RequestBody doctorDTO: DoctorDTO): ResponseEntity<DoctorDTO>? {
+
+        return service.updateDoctor(doctorDTO)
+
+    }
 
 }
--- src/main/kotlin/com/bartek/clinicadministrationapi/domain/dtos/DoctorDTO.kt
+++ src/main/kotlin/com/bartek/clinicadministrationapi/domain/dtos/DoctorDTO.kt
@@ -1,11 +1,9 @@
 package com.bartek.clinicadministrationapi.domain.dtos
 
 data class DoctorDTO(
-
     var id: Long? = null,
     var firstName: String?,
     var lastName: String?,
     var specialisation: String?
-
 
 )
